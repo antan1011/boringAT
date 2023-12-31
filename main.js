aa83b962e1cffc3aadb22f8dbeea82d55d3154a9
antan <154360385+antan1011@users.noreply.github.com>
2023-12-31 20:33:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import './scss/styles.scss'
-import { Popover } from 'bootstrap';
+import './style.css'
 
-import VMdEditor from '@kangc/v-md-editor';
-import '@kangc/v-md-editor/lib/style/base-editor.css';
-import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
-import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
-
-// Prism
-import Prism from 'prismjs';
-// highlight code
-import 'prismjs/components/prism-json';
-
-VMdEditor.use(vuepressTheme, {
-  Prism,
-});
-
-
+import './styles.scss'
 
 
 const app = createApp(App)
-app.use(VMdEditor)
 app.mount('#app')
 
 document.querySelectorAll('[data-bs-toggle="popover"]')
